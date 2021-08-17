e6fb675af5fc9964f897566c184aa042d66d310b
alansenairj <alansenairj@gmail.com>
2021-08-17 17:29:17 -0300

    start atualizado para imagens no hub docker

--- start.sh
+++ start.sh
@@ -8,13 +8,13 @@ for i in 1
       echo "Iniciando oneagent" 
       exec $run
       echo "Iniciando webapp na URL http://172.19.0.3:3000"
-      docker container run -d --net prodnet --ip=172.19.0.3 --name webapp$i webapp node /app/app/app.js
+      docker container run -d --net prodnet --ip=172.19.0.3 --name webapp$i alanaguinaga/webapp node /app/app/app.js
       echo "Iniciando webapi na URL http://172.19.0.4:8085"
-      docker run -d --net prodnet --ip=172.19.0.4 --name api$i api python3 api.py
+      docker run -d --net prodnet --ip=172.19.0.4 --name api$i alanaguinaga/api python3 api.py
       echo "Iniciando career"
-      docker run -d --net prodnet --ip=172.19.0.5 --name career$i career java -jar /career/app/careerws/jar/career-path-0.0.1-SNAPSHOT.jar
+      docker run -d --net prodnet --ip=172.19.0.5 --name career$i alanaguinaga/career java -jar /career/app/careerws/jar/career-path-0.0.1-SNAPSHOT.jar
       echo "Iniciando market"
-      docker run -d --net prodnet --ip=172.19.0.6 --name market$i market java -Dserver.port=8082 -jar /market/app/mktws/jar/mkt-sentiment-0.0.1-SNAPSHOT.jar
+      docker run -d --net prodnet --ip=172.19.0.6 --name market$i alanaguinaga/market java -Dserver.port=8082 -jar /market/app/mktws/jar/mkt-sentiment-0.0.1-SNAPSHOT.jar
       echo "containers inicados:"
       docker ps -q | grep "$name"
 sleep 3
